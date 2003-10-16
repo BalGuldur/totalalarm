02f3680389c8474d0c20b26af6de711e1d074ff4
Aleksandr Krulov <a.krulov@gmail.com>
2003-10-16 09:19:58 +0400

    add link alarm-answer to model and check

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,3 +1,5 @@
 class Alarm < ActiveRecord::Base
   attr_accessible :creator, :descr, :disabler, :enable
+	
+	has_and_belongs_to_many :answers
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
   attr_accessible :descr, :enable
+
+	has_and_belongs_to_many :alarms
 	belongs_to :granswer
 end
