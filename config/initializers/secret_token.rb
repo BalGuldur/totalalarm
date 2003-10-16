de24f1b897e73bbaa847d762fbb44267a64998b8
Aleksandr Krulov <a.krulov@gmail.com>
2003-10-16 08:42:18 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Totalalarm::Application.config.secret_token = '92cfaa8d2f8a24fd45e7e9372d12c423048760f066e897920d3c2e025f42b716817abf216524c7d86d99e4c57fd15148ddbacf0262341cabd341dff6c1137521'
