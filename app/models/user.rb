ff9e16f5ff7a2f3f2b2c64d843f05cb85dcacc80
Aleksandr Krulov <a.krulov@gmail.com>
2012-06-09 16:11:12 +0400

    adding username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :login, :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+	attr_accessor :login
 end
